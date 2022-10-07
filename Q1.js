76193e94cbef840d34a12c999902c7d3b041e6ac
VK132211 <m.vk.80899@gmail.com>
2022-10-07 09:36:25 +0530

    Q1 Done

---
+++ Q1.js
@@ -0,0 +1,18 @@
+// 1. Write a program to find whether a given year is a leap year or not.
+function leap(year){
+    if(year%400==0){
+        console.log("Given year is a leap year");
+    }
+    else if(year%100==0){
+        console.log("Given year is not a leap year");
+    }
+    else if(year%4==0){
+        console.log("Given year is a leap year");
+
+    }
+    else{
+        console.log("Given year is not a leap year");
+    }
+}
+let year = 2024;
+leap(year);
